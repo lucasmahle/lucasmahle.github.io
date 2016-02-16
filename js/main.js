85eb4bc99da0fc9b3eefd9f193e575d338f635e8
Lucas Mahle <lucassmahle@gmail.com>
2016-02-16 09:29:20 -0200

    Update aboud.md

--- js/main.js
+++ js/main.js
@@ -1,29 +1,41 @@
-  SimpleJekyllSearch({
-  searchInput: document.getElementById('search-input'),
-  resultsContainer: document.getElementById('results-container'),
-  json: '/search.json',
-  searchResultTemplate: '<li><a href="{url}" title="{title}">{title}</a></li>',
-  noResultsText: '<li class="no-results">Nada por aqui</li>',
-  limit: 10,
-  fuzzy: false
+SimpleJekyllSearch({
+	searchInput: document.getElementById('search-input'),
+	resultsContainer: document.getElementById('results-container'),
+	json: '/search.json',
+	searchResultTemplate: '<li><a href="{url}" title="{title}">{title}</a></li>',
+	noResultsText: '<li class="no-results">Nada por aqui</li>',
+	limit: 10,
+	fuzzy: false
 });
 
 $(document).ready(function() {
-  $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
-    $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
-    e.preventDefault();
-  });
+	$('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
+		$('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
+		e.preventDefault();
+	});
 
-  var $age = $("#age");
-  if($age.length > 0){
-    var $born = new Date( $age.data('birth') );
+	;(function(){
+		'use strict';
+		/**
+		 * I'm to lazy to edit every, boring, year.
+		 * And this way I do not have to worry about my age
+		 * My name is Lucas Mahle, brazilian and I am the Universal.
+		 */
+		var $age = $("#age");
 
-    var _calculateAge = function(birthday) { // birthday is a date
-      var ageDifMs = Date.now() - birthday.getTime();
-      var ageDate = new Date(ageDifMs); // miliseconds from epoch
-      return Math.abs(ageDate.getUTCFullYear() - 1970);
-    }
+		if($age.length > 0){
+			var $born, _calculateAge;
 
-    $age.text( _calculateAge($born) );
-  }
+			_calculateAge = function(birthday) {
+				var ageDifMs, ageDate
+
+				ageDifMs = Date.now() - birthday.getTime();
+				ageDate = new Date(ageDifMs);
+
+				return Math.abs(ageDate.getUTCFullYear() - 1970);
+			}
+
+			$age.text( _calculateAge( new Date($age.data('birth')) ) );
+		}
+	}());
 });
